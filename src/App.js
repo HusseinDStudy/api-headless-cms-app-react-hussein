2bce16da6fc14cfab0a28a180ab4d5e00d837cd0
hussein <hussein@hussein.com>
2023-06-29 17:25:30 +0200

    working fetch data from gouv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
 
-function App() {
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [addressData, setAddressData] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const fetchData = async () => {
+    try {
+      const encodedAddress = encodeURIComponent(inputValue);
+      const response = await axios.get(
+        `https://api-adresse.data.gouv.fr/search/?q=${encodedAddress}&type=housenumber&autocomplete=1`
+      );
+      setAddressData(response.data.features);
+      console.log(response.data.features);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +42,41 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="App">
+        <h1>Address Lookup</h1>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={fetchData}>Fetch Address</button>
+        {addressData && (
+          <table>
+            <thead>
+              <tr>
+                <th>Label</th>
+                <th>Street</th>
+                <th>City</th>
+                <th>Postcode</th>
+              </tr>
+            </thead>
+            <tbody>
+              {addressData.map((address) => (
+                <tr key={address.properties.id}>
+                  <td>{address.properties.label}</td>
+                  <td>{address.properties.street}</td>
+                  <td>{address.properties.city}</td>
+                  <td>{address.properties.postcode}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        {/* <StripeCheckout
+          token={handleToken}
+          stripeKey="YOUR_STRIPE_PUBLISHABLE_KEY"
+          amount={100} // Amount in cents ($1)
+          name="Payment Example"
+        /> */}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
