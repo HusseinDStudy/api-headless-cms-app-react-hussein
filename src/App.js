db3a29bde46bf6e23e6124f0c9fb2aa96d550e40
HusseinDStudy <85683989+HusseinDStudy@users.noreply.github.com>
2023-10-24 14:47:14 +0200

    Resolve what is missed in project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [addressData, setAddressData] = useState(null);
+  const [inputValue, setInputValue] = useState('');// Input value For Gouv API Fetch
+  const [addressData, setAddressData] = useState(null);// Gouv API
+  const [addressesOnStrapiDb, setAddresses] = useState([]);// Strapi DB
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const fetchData = async () => {
+  const fetchDataFromGouv = async () => {
     try {
       const encodedAddress = encodeURIComponent(inputValue);
       const response = await axios.get(
         `https://api-adresse.data.gouv.fr/search/?q=${encodedAddress}&type=&autocomplete=1`
       );
       setAddressData(response.data.features);
-      console.log(response.data.features);
     } catch (error) {
       console.error(error);
     }
@@ -26,22 +26,33 @@ const App = () => {
   
   const addToDatabase = async (address) => {
     try{
-      axios.post("http://localhost:1337/api/addresses", {
+      await axios.post("http://localhost:1337/api/addresses", {
         data: {
           label: address.properties.label,
           street: address.properties.street,
           city: address.properties.city,
           postcode: address.properties.postcode,
         },
-      }).then((response) => {
-        console.log(response);
-      }); 
+      });
     } catch (error) {
       console.error(error);
     }
   };
-  
-  
+
+  const fetchDataFromStrapiDB = async () => {
+    try {
+      const response = await axios.get("http://localhost:1337/api/addresses");
+      setAddresses(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDataFromStrapiDB().then((response) => {
+      console.log(response);
+    });
+  }, [addressesOnStrapiDb]);
 
   return (
     <div className="App">
@@ -59,38 +70,70 @@ const App = () => {
           Learn React
         </a>
       </header>
-      <div className="App">
         <h1>Address Lookup</h1>
         <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button onClick={fetchData}>Fetch Address</button>
+        <button onClick={fetchDataFromGouv}>Fetch Address</button>
+        <button onClick={fetchDataFromStrapiDB}>Fetch Strapi Addresses</button>
         {addressData && (
-          <table>
-            <thead>
-              <tr>
-                <th>Label</th>
-                <th>Street</th>
-                <th>City</th>
-                <th>Postcode</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {addressData.map((address) => (
-                <tr key={address.properties.id}>
-                  <td>{address.properties.label}</td>
-                  <td>{address.properties.street}</td>
-                  <td>{address.properties.city}</td>
-                  <td>{address.properties.postcode}</td>
-                  <td>
-                    <button onClick={() => addToDatabase(address)}>Add to Strapi</button>
-                  </td>
+          <div>
+            <h1>Fetched Gouv Data</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Label</th>
+                  <th>Street</th>
+                  <th>City</th>
+                  <th>Postcode</th>
+                  <th></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {
+                  addressData.map((address) => (
+                  <tr key={address.properties.id}>
+                      <td>{address.properties.label}</td>
+                      <td>{address.properties.street}</td>
+                      <td>{address.properties.city}</td>
+                      <td>{address.properties.postcode}</td>
+                      <td>
+                        <button onClick={() => addToDatabase(address)}>Add to Strapi</button>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
         )}
-       
-      </div>
+
+        {addressesOnStrapiDb && (
+            <div>
+              <h1>Fetched Strapi Data</h1>
+              <table>
+                <thead>
+                <tr>
+                  <th>Label</th>
+                  <th>Street</th>
+                  <th>City</th>
+                  <th>Postcode</th>
+                  <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                  addressesOnStrapiDb.map((saved_address) => (
+                    <tr key={saved_address.id}>
+                      <td>{saved_address.attributes.label}</td>
+                      <td>{saved_address.attributes.street}</td>
+                      <td>{saved_address.attributes.city}</td>
+                      <td>{saved_address.attributes.postcode}</td>
+                    </tr>
+                  ))
+                }
+                </tbody>
+              </table>
+            </div>
+        )}
     </div>
   );
 };
